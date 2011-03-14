06a635060c438cee43035399e3f446558e9217ea
João Magalhães <joamag@hive.pt>
2011-03-14 18:26:52 +0000

    new ui code

---
+++ commons_ui_mobile/MBCustomView.h
@@ -0,0 +1,15 @@
+//
+//  MBCustomView.h
+//  commons_ui_mobile
+//
+//  Created by João Magalhães on 3/14/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MBCustomView : UIView {
+    
+}
+
+@end
---
+++ commons_ui_mobile/MBCustomView.m
@@ -0,0 +1,14 @@
+//
+//  MBCustomView.m
+//  commons_ui_mobile
+//
+//  Created by João Magalhães on 3/14/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "MBCustomView.h"
+
+
+@implementation MBCustomView
+
+@end
