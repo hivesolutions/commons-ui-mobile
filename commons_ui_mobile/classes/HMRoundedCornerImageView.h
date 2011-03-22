8aff40e45d183659a4d3bfd81662545cdba5523d
Tiago Silva <tsilva@hive.pt>
2011-03-22 11:04:42 +0000

    added rounded corner image view

--- commons_ui_mobile/classes/HMRoundedCornerImageView.h
+++ commons_ui_mobile/classes/HMRoundedCornerImageView.h
@@ -16,32 +16,16 @@
 // You should have received a copy of the GNU General Public License
 // along with Hive Mobile. If not, see <http://www.gnu.org/licenses/>.
 
-// __author__    = João Magalhães <joamag@hive.pt>
+// __author__    = Tiago Silva <tsilva@hive.pt>
 // __version__   = 1.0.0
 // __revision__  = $LastChangedRevision: 2390 $
 // __date__      = $LastChangedDate: 2009-04-02 08:36:50 +0100 (qui, 02 Abr 2009) $
 // __copyright__ = Copyright (c) 2008 Hive Solutions Lda.
 // __license__   = GNU General Public License (GPL), Version 3
 
-// dependencies includes
-#import "../classes/Dependencies.h"
+#import "Dependencies.h"
 
-// class includes
-#import "../classes/HMDateTableViewCell.h"
-#import "../classes/HMEditTableViewCell.h"
-#import "../classes/HMEditTableViewCellEditView.h"
-#import "../classes/HMHeaderItemTableView.h"
-#import "../classes/HMItemTableView.h"
-#import "../classes/HMItemTableViewDelegate.h"
-#import "../classes/HMItemTableViewProvider.h"
-#import "../classes/HMItemTableViewDataSource.h"
-#import "../classes/HMRemoteTableView.h"
-#import "../classes/HMRemoteTableViewDelegate.h"
-#import "../classes/HMRemoteTableViewProvider.h"
-#import "../classes/HMRemoteTableViewDataSource.h"
-#import "../classes/HMStringTableViewCell.h"
-#import "../classes/HMTableViewCell.h"
-#import "../classes/HMTableViewCellBackgroundView.h"
-#import "../classes/HMRemoteItemTableViewController.h"
-#import "../classes/HMRemoteHeaderItemTableViewController.h"
-#import "../classes/HMRoundedCornerImageView.h"
+@interface HMRoundedCornerImageView : UIImageView {
+}
+
+@end
